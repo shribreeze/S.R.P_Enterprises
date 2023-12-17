3f00c727dfc528ea7163675790b4c28a66b13431
Sameer Gautam <sg10682002@gmail.com>
2023-12-17 22:30:50 +0530

    applying filter

--- product.js
+++ product.js
@@ -24,3 +24,41 @@ document.onreadystatechange = function() {
       navbar.classList.remove('navbar-red');
   });
 }
+
+var filters = {
+  type: 'all'
+};
+
+function toggleFilter(type, value) {
+  if (filters[type] === value) {
+    // If the same button is clicked again, reset the filter for that type
+    filters[type] = 'all';
+  } else {
+    filters[type] = value;
+  }
+
+  applyFilters();
+}
+function applyFilters() {
+  var items = document.querySelectorAll('.item');
+
+  items.forEach(function (item) {
+    var itemType = item.getAttribute('data-type');
+    // var itemSize = item.getAttribute('data-size');
+
+    var typeCondition = filters.type === 'all' || filters.type === itemType;
+    // var sizeCondition = filters.size === 'all' || filters.size === itemSize;
+
+    if (typeCondition) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
+function resetFilters() {
+  filters.type = 'all';
+  // filters.size = 'all';
+  applyFilters();
+}
