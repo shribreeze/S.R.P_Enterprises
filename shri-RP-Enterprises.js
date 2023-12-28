dd4dab34c0cd2ab575e52890dc4d709274bc70db
Sameer Gautam <sg10682002@gmail.com>
2023-12-28 17:07:44 +0530

    added shree foils and changed name of rp ent

---
+++ shri-RP-Enterprises.js
@@ -0,0 +1,26 @@
+// Toggle mobile menu visibility
+const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+const closeMobileMenuBtn = document.getElementById('close-mobile-menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
+
+closeMobileMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.add('hidden');
+});
+
+//navbar scroll effect
+document.onreadystatechange = function() {
+  let lastScrollPosition = 0;
+  const navbar = document.querySelector('.navbar');
+  window.addEventListener('scroll', function(e) {
+    lastScrollPosition = window.scrollY;
+    
+    if (lastScrollPosition > 20)
+      navbar.classList.add('navbar-red');
+    else
+      navbar.classList.remove('navbar-red');
+  });
+}
